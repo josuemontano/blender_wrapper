d30b61a82533347f8ea2d0250e3d5346e4dcbc07
Josue Montano <josuemontanoa@gmail.com>
2017-05-26 21:07:32 -0400

    Create variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+LAYER_1 = (True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False)
+LAYER_2 = (False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False)
+LAYER_3 = (False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False)
+LAYER_4 = (False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False)
+
+ORIGIN = (0, 0, 0)
